66ef994ba3d6ab6e4d40899d02cfe956b61cf733
YKChang92 <71055268+YKChang92@users.noreply.github.com>
2020-09-10 11:23:42 +0800

    Add files via upload

---
+++ densenet.py
@@ -0,0 +1,178 @@
+import torch
+import os
+import torch.nn as nn
+import torch.optim as optim
+
+import torch.nn.functional as F
+
+import math
+
+from torchviz import make_dot
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+class SingleLayer(nn.Module):
+    def __init__(self, inchannel, outchannel):
+        super(SingleLayer, self).__init__()
+        self.bn1 = nn.BatchNorm2d(inchannel)
+        self.conv1 = nn.Conv2d(inchannel, outchannel, kernel_size=5,padding=2)
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = torch.cat((x, out), 1)
+        return out
+
+
+
+
+class Transition(nn.Module):
+    def __init__(self, inchannel, outchannel):
+        super(Transition, self).__init__()
+        self.bn1 = nn.BatchNorm2d(inchannel)
+        self.conv1 = nn.Conv2d(inchannel,outchannel, kernel_size=1)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        return out
+
+class SDB( nn.Module):
+    def __init__(self):
+        super(SDB, self).__init__()
+        self.bn=nn.BatchNorm2d(64)
+        self.conv=nn.Conv2d(64, 32, 5, padding=2)
+        self.dense1=self._make_dense()
+        self.dense2 = self._make_dense()
+        self.trans1=Transition(192, 32)
+        self.trans2 = Transition(192, 32)
+
+    def _make_dense(self):
+        layers = []
+        layers.append(SingleLayer(32,32))
+        layers.append(SingleLayer(64, 32))
+        layers.append(SingleLayer(96, 32))
+        layers.append(SingleLayer(128, 32))
+        return nn.Sequential(*layers)
+
+    def forward(self,x,y):
+        s = F.relu(self.bn(torch.cat([x, y], 1)))
+        s = self.conv(s)
+        s1 = self.trans1(torch.cat([x,self.dense1(s)],1))
+        s2 = self.trans2(torch.cat([y,self.dense2(s)],1))
+        return s1,s2
+
+class IFB( nn.Module):
+    def __init__(self):
+        super(IFB, self).__init__()
+        self.dense = self._make_dense_2()
+        self.trans = Transition(192, 64)
+    def _make_dense_2(self):
+        layers = []
+        layers.append(SingleLayer(64, 32))
+        layers.append(SingleLayer(96, 32))
+        layers.append(SingleLayer(128, 32))
+        layers.append(SingleLayer(160, 32))
+        return nn.Sequential(*layers)
+    def forward(self,x):
+        out=self.trans(self.dense(x))
+        return out
+
+
+
+
+
+class SDNet(nn.Module):
+    def __init__(self):
+        super(SDNet, self).__init__()
+
+        self.conv1_0= nn.Conv2d(3, 32, 3, padding=1)
+        self.conv1_1 =nn.Conv2d(3, 32, 3, padding=1)
+        self.SDB0 = SDB()
+        self.SDB1 = SDB()
+        self.SDB2 = SDB()
+        self.SDB3 = SDB()
+        self.SDB4 = SDB()
+        self.SDB5 = SDB()
+        self.SDB6 = SDB()
+
+        self.conv3_1=nn.Conv2d(256, 32, 5, padding=2)
+        self.conv3_2 = nn.Conv2d(256, 32, 5, padding=2)
+        self.conv4_1 = nn.Conv2d(32, 3, 5, padding=2)
+        self.conv4_2 = nn.Conv2d( 32, 3,5, padding=2)
+        self.bn1_1=nn.BatchNorm2d(32)
+        self.bn1_2= nn.BatchNorm2d(32)
+        self.bn2_1=nn.BatchNorm2d(256)
+        self.bn2_2 = nn.BatchNorm2d(256)
+        # for m in self.modules():
+        #     if isinstance(m, nn.Conv2d):
+        #         n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        #         m.weight.data.normal_(0, math.sqrt(2. / n))
+        #     elif isinstance(m, nn.BatchNorm2d):
+        #         m.weight.data.fill_(1)
+        #         m.bias.data.zero_()
+        #     elif isinstance(m, nn.Linear):
+        #         m.bias.data.zero_()
+    def forward(self, x,y):
+        s1_0_0 = self.conv1_0(x)
+        s2_0_0 = self.conv1_1(y)
+
+        s1_0,s2_0=self.SDB0(s1_0_0,s2_0_0)
+
+        s1_1, s2_1 = self.SDB1(s1_0_0, s2_0_0)
+        s1_1_0= F.avg_pool2d(s1_1 ,2)
+        s2_1_0 = F.avg_pool2d(s2_1, 2)
+
+        s1_2, s2_2 = self.SDB2(s1_1_0, s2_1_0)
+        s1_2_0=F.interpolate(s1_2,(224,320),mode='bilinear')
+        s2_2_0 = F.interpolate(s2_2, (224, 320), mode='bilinear')
+
+        s1_3, s2_3 = self.SDB3(s1_2, s2_2)
+        s1_3_0 = F.interpolate(s1_3, (224, 320), mode='bilinear')
+        s2_3_0 = F.interpolate(s2_3, (224, 320), mode='bilinear')
+
+        s1_4, s2_4 = self.SDB4(s1_3, s2_3)
+        s1_4 =F.interpolate(s1_4,(224,320),mode='bilinear')
+        s2_4 =F.interpolate(s2_4,(224,320),mode='bilinear')
+        s1_5, s2_5 = self.SDB5(s1_4, s2_4)
+        s1_6, s2_6 = self.SDB6(s1_5, s2_5)
+        s1=torch.cat([s1_0_0, s1_0, s1_1, s1_2_0, s1_3_0, s1_4, s1_5, s1_6], 1)
+        s2=torch.cat([s2_0_0, s2_0, s2_1, s2_2_0, s2_3_0, s2_4, s2_5, s2_6], 1)
+        s1=self.conv4_1(self.bn1_1(self.conv3_1(self.bn2_1(s1))+s1_0_0))
+        s2=self.conv4_2(self.bn1_2(self.conv3_2(self.bn2_2(s2))+s2_0_0))
+        return s1,s2
+
+class IFNet(nn.Module):
+    def __init__(self):
+        super(IFNet, self).__init__()
+        self.IFB0 = IFB()
+        self.IFB1 = IFB()
+        self.IFB2 = IFB()
+        self.conv1=nn.Conv2d(6,64,7,padding=3)
+        self.conv2 = nn.Conv2d(256, 64, 7, padding=3)
+        self.bn1=nn.BatchNorm2d(256)
+        self.bn2=nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d( 64, 3,7, padding=3)
+    def forward(self, a,b):
+        x=torch.cat([a,b],1)
+        x_0=self.conv1(x)
+        x_1=self.IFB0(x_0)
+        x_2=self.IFB1(x_1)
+        x_3=self.IFB2(x_2)
+        x_4= torch.cat([x_0,x_1,x_2,x_3],1)
+        x=self.conv2(F.relu(self.bn1(x_4)))
+        output=self.conv3(self.bn2(x))
+        return output
+
+
+
+
+
+
+
+
+# dd=IFNet()
+#
+# print(dd)
+# # # g=make_dot(dd)
+# # # g.render('espnet_model', view=False)
+# a=torch.ones(1,3,224,320)
+# b=torch.ones(1,3,224,320)
+#
+# c,d=dd(a,b)
+# print(c.size())
---
+++ tests.py
@@ -0,0 +1,66 @@
+import torch
+import numpy as np
+from pytorch_msssim import  SSIM
+import os
+import skimage.io as io
+from densenet import SDNet, IFNet
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+log_dir='/home/changyakun_16/raid/log/'
+str1='/data/im1/'
+str2='/data/im2/'
+str3='/data/im3/'
+str4='/data/im4/'
+str5='/data/im5/'
+net_dict='/parameters/net_final.pth'
+net2_dict='/parameters/net2_final.pth'
+#result_dir='/home/changyakun_16/raid/result/'
+
+#lis=np.load('/home/changyakun_16/ref/test_list3.npy')
+#lis=np.load('/home/changyakun_16/ref/test_list1.npy')
+lis=np.load('/home/changyakun_16/ref/test_list2.npy')
+def trans(input):
+    return np.float32(np.transpose(np.expand_dims(input,-1),(3,2,0,1)))
+def read_image(s):
+    s=str(s)
+    im_a=trans(io.imread(str1+s+'.png'))
+    im_f=trans(io.imread(str2+s+'.png'))
+    g_a = trans(io.imread(str3 + s + '.png'))
+    g_f = trans(io.imread(str4 + s + '.png'))
+    G_f = trans(io.imread(str5 + s + '.png'))
+    return im_a,im_f,g_a,g_f,G_f
+
+net = SDNet()
+net2=IFNet()
+net=torch.nn.DataParallel(net)
+net2=torch.nn.DataParallel(net2)
+net.cuda()
+net2.cuda()
+net.load_state_dict(torch.load(net_dict))
+net2.load_state_dict(torch.load(net2_dict))
+ssim = SSIM(data_range=255, size_average=True, channel=3)
+S_a=0
+S_f=0
+S_c=0
+for i in range(0, np.shape(lis)[1]):
+    im_a,im_f,g_a,g_f,G_f = read_image(lis[0,i])
+    im_a,im_f,g_a,g_f,G_f=torch.tensor(im_a,dtype=torch.float32),torch.tensor(im_f),torch.tensor(g_a),torch.tensor(g_f),torch.tensor(G_f)
+    im_a, im_f, g_a, g_f, G_f = im_a.cuda(), im_f.cuda(), g_a.cuda(), g_f.cuda(), G_f.cuda()
+    net.eval()
+    net2.eval()
+    out_a, out_f = net(im_a - 90.5, im_f - 120.5)
+    output = net2(out_a, out_f)
+    output=torch.clamp(output,0,255)
+    s1=torch.mean(ssim(out_a , g_a))
+    s2=torch.mean(ssim(out_f , g_f))
+    s3=torch.mean(ssim(output, G_f))
+    S_a+=s1.item()
+    S_f+=s2.item()
+    S_c+=s3.item()
+
+    # io.imsave(result_dir+str(lis[i])+'_1.png',np.transpose(np.squeeze(out_a.cpu().detach().numpy()),(1,2,0)))
+    # io.imsave(result_dir + str(lis[i]) + '_2.png',np.transpose(np.squeeze(out_f.cpu().detach().numpy()),(1,2,0)))
+    # io.imsave(result_dir + str(lis[i]) + '_3.png', np.transpose(np.squeeze(output.cpu().detach().numpy()),(1,2,0)))
+
+    print('s1: %.6f,  s2: %.6f,   s3: %.6f' % (s1.item(),s2.item(),s3.item()))
+
+print(S_c/np.shape(lis)[1])
